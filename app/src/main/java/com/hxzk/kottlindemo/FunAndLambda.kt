3e2b1fc449ecbb7bc4a7870de66c852f64da899a
SearchDingDing <15910513547@163.com>
2019-12-01 17:00:26 +0800

    类与对象-函数完成

--- app/src/main/java/com/hxzk/kottlindemo/FunAndLambda.kt
+++ app/src/main/java/com/hxzk/kottlindemo/FunAndLambda.kt
@@ -1,7 +1,9 @@
 package com.hxzk.kottlindemo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.hxzk.kottlindemo.function_lambda.FunctionActivity
 import kotlinx.android.synthetic.main.activity_fun_and_lambda.*
 
 class FunAndLambdaActivity : AppCompatActivity() {
@@ -9,7 +11,9 @@ class FunAndLambdaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fun_and_lambda)
-        btn_funciton.setOnClickListener {  }
+        btn_funciton.setOnClickListener {
+            startActivity(Intent(this,FunctionActivity :: class.java))
+        }
         btn_lambda.setOnClickListener {  }
         btn_innerfunciont.setOnClickListener {  }
 
---
+++ app/src/main/java/com/hxzk/kottlindemo/function_lambda/FunctionActivity.kt
@@ -0,0 +1,13 @@
+package com.hxzk.kottlindemo.function_lambda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hxzk.kottlindemo.R
+
+class FunctionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_function)
+    }
+}
